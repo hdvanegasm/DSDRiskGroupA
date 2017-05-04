ccf7d42858c6ee732e47063f2f7350166e18be35
root <root@localhost>
2017-05-04 17:42:57 -0500

    Create account service

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -5,6 +5,11 @@
  */
 package server;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import static spark.Spark.post;
+
 /**
  *
  * @author root
@@ -12,5 +17,21 @@ package server;
 public class Server {
     public static void main(String[] args) {
         // TODO Implement services
+        
+        post("/createAccount", (request, response) -> {
+            JSONParser parser = new JSONParser();
+            String jsonToString = "["+ request.body() +"]";           
+            Object obj = parser.parse(jsonToString);
+            JSONArray jsonArray = (JSONArray) obj;
+
+            JSONObject parsedObject = (JSONObject) jsonArray.get(0);
+            
+            //TODO Get attributes
+            String username = (String) parsedObject.get("username");
+            
+            //TODO get response from DBA
+            return true;
+
+        });
     }
 }
