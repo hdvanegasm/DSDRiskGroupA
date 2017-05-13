5d1a26d3c3ae7efc1d3af40871bf2bb9420a7fb9
hernan232 <hdvanegasm@unal.edu.co>
2017-05-12 22:47:22 -0500

    Add code documentation to RequestState and Session classes (Session class is not finihed yet).

--- src/main/java/server/accountmanager/model/Session.java
+++ src/main/java/server/accountmanager/model/Session.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server.accountmanager.model;
 
 import java.util.LinkedList;
 
 /**
- *
+ * This class represents a session in the game. It has the basic attributes in order to create and manage a session in the game.
  * @author Hernán Darío Vanegas Madrigal
  */
 public class Session {
@@ -20,6 +15,13 @@ public class Session {
     public LinkedList<Player> players;
     public SessionState state;
 
+    /**
+     * The constructor of the session is based on basic attributes. It allows to create a fill the basic attributes of a session.
+     * @param id It is the identification of the session. This identification is unique in the system and it can be used as a reference for the players.
+     * @param numberOfPlayers This attribute store the number of players in the session.
+     * @param type It represents the type of game that will be played. Each type of game has different rules and number of players.
+     * @param state This attribute shows what is the actual state of the session. A session can be in a "creating" status, a "playing" status (this status is selected when the host pushes the "Start game" button), and "finished". 
+     */
     private Session(int id, int numberOfPlayers, SessionType type, SessionState state) {
         this.id = id;
         this.numberOfPlayers = numberOfPlayers;
