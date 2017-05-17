54a7de92712cd1ae212118b007150bcd3243c881
hernan232 <hdvanegasm@unal.edu.co>
2017-05-17 16:54:29 -0500

    Add commentaries to Session Builder

--- src/main/java/server/DatabaseConnector.java
+++ src/main/java/server/DatabaseConnector.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package server;
 
 import java.sql.Connection;
@@ -11,24 +7,32 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- *
- * @author lemark
+ * This class is a connection channel between the program and the database. It uses the singleton pattern in order to ensure the existence of one instance of the class
+ * @author Hernan Darío Vanegas Madrigal
  */
 public class DatabaseConnector {
 
+    // Server port and database information 
     private int port = 3306;
     private String serverPath;
     private String dataBaseName;
     private String dataBaseUser;
     private String dataBasePassword;
 
+    // Connection instances with JDBC
     private String driver;
     private String dataBase;
     private Connection connection;
-    private Statement statement;
-
+    private Statement statement;    
+    
+    // Singleton instance
     private static DatabaseConnector instance;
-
+    
+    /**
+     * This method constructs the instance of the connector with the specified requirements and information of the server.
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
     public DatabaseConnector() throws ClassNotFoundException, SQLException {
         port = 3306;
         serverPath = "localhost";
@@ -44,6 +48,12 @@ public class DatabaseConnector {
         statement = connection.createStatement();
     }
 
+    /**
+     * Singleton method of the class in order to ensure a unique instance.
+     * @return The method returns the instance of the class. It is important to notice that this instance is unique in the server.
+     * @throws ClassNotFoundException
+     * @throws SQLException 
+     */
     public static DatabaseConnector getInstance() throws ClassNotFoundException, SQLException {
         if (instance == null) {
             instance = new DatabaseConnector();
@@ -53,6 +63,10 @@ public class DatabaseConnector {
         }
     }
 
+    /**
+     * Getter method to access to statement object. This object is necessary to execute some queries.
+     * @return The method returns a reference to the statement object that will be used to execute some queries.
+     */
     public Statement getStatement() {
         return statement;
     }
--- src/main/java/server/gamebuilder/controller/SessionBuilder.java
+++ src/main/java/server/gamebuilder/controller/SessionBuilder.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server.gamebuilder.controller;
 
 
@@ -19,19 +14,14 @@ import server.gamebuilder.model.SessionState;
 
 
 /**
- *
+ * 
  * @author David Ochoa
  */
 public class SessionBuilder {
 
-    //connection attributes
-
-
-    
-  
-
     public static boolean createSession(User user, Session session) {
         Host host = new Host(user.account);
+        
         // get actual session id and update in the class
         String querySelectId = "SELECT MAX(id) as id FROM session";
         try {
