e92a723d20f767007c0d832bf312d7e86c845075
hernan232 <hdvanegasm@unal.edu.co>
2017-05-23 13:35:21 -0500

    Define methods for in the module package

--- src/main/java/server/gamebuilder/model/Player.java
+++ src/main/java/server/gamebuilder/model/Player.java
@@ -14,16 +14,20 @@ public class Player extends User {
     /**
      * This method allows to create a player using a given account. This is in order to create a player based on a existing user.
      * @param account This is the base account to create a player. In a normal process, this account belongs to the user that will be transformed to a player when he joins to a session.
+     * @param color This attribute represents the color assigned to the session
      */
-    public Player(Account account) {
+    public Player(Account account, Color color) {
         super(account);
     }
+ 
     
     /**
      * This method is used to take out a player from a session in the construction phase.
+     * @param session This parameter represents the session in which the user will be deleted
      * @return It returns true if the user was successfully taken out from the session, otherwise it returns false. 
      */
-    public boolean takeOut() {
+    public boolean takeOut(Session session) {
+        session.players.remove(this);
         return true;
     }
 
