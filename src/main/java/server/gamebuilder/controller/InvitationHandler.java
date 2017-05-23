8435ed97a794708068c614d2f69665ea44ccde24
hernan232 <hdvanegasm@unal.edu.co>
2017-05-22 23:40:30 -0500

    Add method structure for second increment

--- src/main/java/server/gamebuilder/controller/InvitationHandler.java
+++ src/main/java/server/gamebuilder/controller/InvitationHandler.java
@@ -1,9 +1,20 @@
 package server.gamebuilder.controller;
 
+import server.DatabaseConnector;
+import server.accountmanager.model.User;
+
 /**
- * This class handles the invitations provided by host to his contacts.
+ * This class handles the invitations provided by host to his contacts. This class acts as a controller that makes the connection with the database in order to manage the invitations
  * @author Hernán Darío Vanegas Madrigal
  */
 public class InvitationHandler {
+    // TODO Complete parameters
+    public static boolean inviteContact(User user){
+        return true;
+    }
     
+    // TODO Complete parameters
+    public static boolean answerInvitation() {
+        return true;
+    }
 }
--- src/main/java/server/gamebuilder/controller/RequestHandler.java
+++ src/main/java/server/gamebuilder/controller/RequestHandler.java
@@ -1,10 +1,21 @@
 
 package server.gamebuilder.controller;
 
+import server.accountmanager.model.User;
+import server.gamebuilder.model.Request;
+import server.gamebuilder.model.Session;
+import server.DatabaseConnector;
+
 /**
  * This class handles the requests and route the answers to database and the host of the session
  * @author Hernán Darío Vanegas Madrigal
  */
 public class RequestHandler {
+    public static boolean makeRequest(User username, Session session) {
+        return true;
+    }
     
+    public boolean answerRequest(Request request) {
+        return true;
+    }
 }
